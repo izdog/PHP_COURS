a626f269f67b3f5a1757f9c922a99d1933bed34f
Florian FRIDEL <florian.fridel@yschools.eu>
2019-07-03 11:11:36 +0200

    Ajout des fichiers de base

---
+++ data/datas.php
@@ -0,0 +1,87 @@
+<?php
+
+$movies = [
+    [
+        'id' => '1',
+        'title' => 'Aliens',
+        'director' => 'James Cameron',
+        'type' => 'Science-fiction',
+        'synopsis' => "Après 57 ans de dérive dans l'espace, Ellen Ripley est secourue par la corporation Weyland-Yutani. Malgré son rapport concernant l’incident survenu sur le Nostromo, elle n’est pas prise au sérieux par les militaires quant à la présence de xénomorphes sur la planète LV-426 où se posa son équipage… planète où plusieurs familles de colons ont été envoyées en mission de 'terraformage'. Après la disparition de ces derniers, Ripley décide d'accompagner une escouade de marines dans leur mission de sauvetage... et d’affronter à nouveau la Bête.",
+        'year' => '1986'
+    ],
+    [
+        'id' => '2',
+        'title' => "Child's play",
+        'director' => 'Tom Holland',
+        'type' => 'Horror',
+        'synopsis' => "Pour ses six ans, Andy Barclay est comblé, sa mère lui fait cadeau d'une poupee parlante, joufflue à souhait nommée Chucky. Andy est fou de sa poupée et lui seul connait son secret. Chucky sait faire bien des choses, elle est même capable de tuer...",
+        'year' => '1988'
+    ],
+    [
+        'id' => '3',
+        'title' => "The Goonies",
+        'director' => 'Richard Donner',
+        'type' => 'Adventure',
+        'synopsis' => "Astoria est une modeste et paisible cité portuaire de la Côte Ouest. Trop paisible au gré de ses jeunes habitants... 'Il ne se passe jamais rien ici', soupira un jour l'un d'eux, mélancolique. Mickey Walsh, treize ans, venait de prononcer une de ces phrases fatidiques qui annoncent parfois les aventures les plus étranges, les plus folles et les plus amusantes.",
+        'year' => '1985' 
+    ],
+    [
+        'id' => '4',
+        'title' => "Enter the void",
+        'director' => 'Gaspar Noé',
+        'type' => 'Drama Fantasy',
+        'synopsis' => "Oscar et sa sœur Linda habitent depuis peu à Tokyo. Oscar survit de petits deals de drogue alors que Linda est stripteaseuse dans une boite de nuit. Un soir, lors d'une descente de police, Oscar est touché par une balle. Tandis qu'il agonise, son esprit, fidèle à la promesse faite à sa sœur de ne jamais l'abandonner, refuse de quitter le monde des vivants. Son esprit erre alors dans la ville et ses visions deviennent de plus en plus chaotiques et cauchemardesques. Passé, présent et futur se mélangent dans un maelstrom hallucinatoire.",
+        'year' => '2010' 
+    ],
+    [
+        'id' => '5',
+        'title' => "Morse",
+        'director' => 'Tomas Alfredson',
+        'type' => 'Fantasy Horror',
+        'synopsis' => "Oskar est un adolescent fragile et marginal, totalement livré à lui-même et martyrisé par les garçons de sa classe. Pour tromper son ennui, il se réfugie au fond de la cour enneigée de son immeuble, et imagine des scènes de vengeance. Quand Eli s'installe avec son père sur le même pallier que lui, Oskar trouve enfin quelqu'un avec qui se lier d'amitié. Ne sortant que la nuit, et en t-shirt malgré le froid glacial, la jeune fille ne manque pas de l'intriguer... et son arrivée dans cette banlieue de Stockolm coïncide avec une série de morts sanglantes et de disparitions mystérieuses. 
+        Il n'en faut pas plus à Oskar pour comprendre : Eli est un vampire. Leur complicité n'en pâtira pas, au contraire...",
+        'year' => '2008'
+    ],
+    [
+        'id' => '6',
+        'title' => "Mad Max 2",
+        'director' => 'George Miller',
+        'type' => 'SF post-apo',
+        'synopsis' => "Dans un futur non défini, les réserves de pétrole sont épuisées et la violence règne sur le monde. Max, un ancien de la sécurité routière, se porte aux secours d'une communauté de fuyards aux prises avec des pirates de la route. La bataille se concentre autour d'une citerne de raffinerie.",
+        'year' => '1981'
+    ],
+    [
+        'id' => '7',
+        'title' => "Moonrise Kingdom",
+        'director' => 'Wes Anderson',
+        'type' => 'Comédie dramatique',
+        'synopsis' => "Sur une île au large de la Nouvelle-Angleterre, au cœur de l’été 1965, Suzy et Sam, douze ans, tombent amoureux, concluent un pacte secret et s’enfuient ensemble. Alors que chacun se mobilise pour les retrouver, une violente tempête s’approche des côtes et va bouleverser davantage encore la vie de la communauté.",
+        'year' => '2012'
+    ],
+    [
+        'id' => '8',
+        'title' => "Sorcerer",
+        'director' => 'William Friedkin',
+        'type' => 'Thriller',
+        'synopsis' => "Quatre étrangers de nationalités différentes, chacun recherché dans son pays, s'associent pour conduire un chargement de nitroglycérine à travers la jungle sud-américaine… 
+        Un voyage au coeur des ténèbres…",
+        'year' => '1977'
+    ],
+    [
+        'id' => '9',
+        'title' => "Wake in fright",
+        'director' => 'Ted Kotcheff',
+        'type' => 'Drama',
+        'synopsis' => "John Grant, un jeune instituteur, fait escale dans une petite ville minière de Bundayabba avant de partir en vacances à Sydney. Le soir, il joue son argent et se soûle. Ce qui devait être l'affaire d'une nuit s'étend sur plusieurs jours...",
+        'year' => '1971'
+    ]
+];
+
+$genres = [
+    'action',
+    'aventure',
+    'drame',
+    'fantastique',
+    'horreur',
+    'science-fiction'
+];
---
+++ movie.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Mr Bidule - Mad Max 2</title>
+</head>
+<body>
+    <!-- NAVBAR   -->
+
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <a class="navbar-brand" href="index.php">Mr Bidule</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../admin/">Admin</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+
+    <!-- FIN NAVBAR -->
+    
+        <div class="container">
+            <div class="row">
+
+            <!-- FILM  -->
+                <div class="col-9">
+                        <article>
+                            <h1>Mad Max 2</h1>
+                            <hr>
+                            <div class="article__content">
+                                <span>Genres : </span>SF post-apo 
+                                <span>Année : </span>1981
+                                <span>Réalisateur :</span>George Miller
+                                <p>Dans un futur non défini, les réserves de pétrole sont épuisées et la violence règne sur le monde. Max, un ancien de la sécurité routière, se porte aux secours d'une communauté de fuyards aux prises avec des pirates de la route. La bataille se concentre autour d'une citerne de raffinerie</p>
+                            </div>
+                        </article>
+                </div>
+            <!-- FIN FILM  -->
+
+            <!-- SECOND NAV GENRE -->
+                <div class="col-3">
+                    <aside>
+                        <h1>Genres</h1>
+                        <hr>
+                        <ul>
+
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Aventure</a></li>
+                            <li><a href="#">Drame</a></li>
+                            <li><a href="#">Fantastique</a></li>
+                            <li><a href="#">Horreur</a></li>
+                            <li><a href="#">Science-fiction</a></li>
+                            
+                        </ul>
+                    </aside>
+                </div>
+            <!-- FIN SECOND NAV GENRE  -->
+
+            </div>
+        </div>
+    <!-- FOOTER -->
+        <footer>
+            <p>Copyright MrBidule</p>
+        </footer>
+    <!-- FIN FOOTER -->
+
+<!-- SCRIPT JAVASCRIPT -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<!-- FIN SCRIPT JAVASCRIPT -->  
+
+</body>
+</html>
